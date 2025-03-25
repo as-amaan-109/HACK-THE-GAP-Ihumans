43b4facdf1491a9acc35762d82f9f6475e2a93b2
jackop <lolits2626@gmail.com>
2025-03-25 14:51:56 +0530

    2nd commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,20 @@
 // "use client"
 
 import "./globals.css";
-// import UiProvider from "./components/UiProvider";
+import UiProvider from "./components/UiProvider";
 
-// import StoreProvider from "./components/StoreProvider";
+import StoreProvider from "./components/StoreProvider";
 import { Toaster } from "react-hot-toast";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`antialiased`}>
-        {/* <UiProvider>
+        <UiProvider>
           <StoreProvider>{children}</StoreProvider>
           <Toaster/>
           
-        </UiProvider> */}
-        {children}
+        </UiProvider>
+        {/* {children} */}
       </body>
     </html>
   );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
 "use client";
 
-// import Image from "next/image";
-// import Header from "./components/Header";
-// import { Search } from "lucide-react";
-// import CoursesGridView from "./components/CoursesGridView";
+import Image from "next/image";
+import Header from "./components/Header";
+import { Search } from "lucide-react";
+import CoursesGridView from "./components/CoursesGridView";
 import { useState } from "react";
 
 export default function Home() {
@@ -11,14 +11,14 @@ export default function Home() {
 
   return (
     <>
-      {/* <Header /> */}
+      <Header />
       <section className="w-full bg-fixed bg-[url('https://i.ibb.co/DPBgHTG5/Screenshot-2025-03-12-000505.png')] h-[400px] bg-no-repeat bg-cover bg-top">
         <div className="flex flex-col justify-center items-center w-full h-[100%] bg-black bg-opacity-35">
           <h1 className="text-white text-xl backdrop-blur-md px-4 py-1 rounded-full font-semibold">
             All the Skills you need in one place
           </h1>
           <form className="relative">
-            {/* <Search className="absolute left-2 top-1/2 -translate-y-1/2 text-purple-400" /> */}
+            <Search className="absolute left-2 top-1/2 -translate-y-1/2 text-purple-400" />
             <input
               type="text"
               placeholder="search for course"
@@ -29,7 +29,7 @@ export default function Home() {
           </form>
         </div>
       </section>
-      {/* <CoursesGridView search={search} /> */}
+      <CoursesGridView search={search} />
     </>
   );
 }
